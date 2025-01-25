fd38abdd82068cc026f39eebb735911d1813d2f4
MesonProgrammer <quantaeli101@gmail.com>
2025-01-25 16:20:39 +0100

    Remove authentication code
    Authentication code is removed since it is unused in the scope of this project.

--- main.py
+++ main.py
@@ -8,18 +8,7 @@ COOKIE = os.getenv("COOKIE")
 
 class Bot:
     def __init__(self) -> None:
-        self.xcsrf_token = self.get_xcsrf_token()
-
-    def get_xcsrf_token(self) -> str:
-        headers = {
-            'cookie': COOKIE
-        }
-
-        response = requests.post('https://auth.roblox.com/v2/logout', headers=headers)
-
-        x_csrf_token = response.headers.get('x-csrf-token', '')
-
-        return x_csrf_token
+        pass
     
     def check_presence(self, user_id: int) -> Literal['offline'] | Literal['online'] | Literal['gaming'] | Literal['unknown']:
         json = {
@@ -56,31 +45,5 @@ class Bot:
         else:
             raise Exception(f"Error occurred. Status code: {response.status_code}. Text: {response.text}")
 
-def check_env() -> None:
-    # Checks if the .env file exists
-    if os.path.exists('.env'):
-        return
-    
-    # If it doesn't it will proceed to generate the file
-    print("No .env file exists. Proceeding with .env file generation protocol.")
-    
-    # Allows input for the cookie
-    while True:
-        cookie = input("Insert cookie here: ").strip()
-        if cookie: break
-        print("Cookie cannot be empty. Try again.")
-
-    # Ensures the cookie starts with ".ROBLOSECURITY="
-    if not cookie.startswith(".ROBLOSECURITY="):
-        cookie = ".ROBLOSECURITY=" + cookie
-    
-    # Writes the cookie content into the new .env file
-    try:
-        with open('.env', 'w') as f:
-            f.write(f"COOKIE={cookie}")
-    except Exception as e:
-        print(f"An error occurred while creating the .env file: {e}")
-
 if __name__ == '__main__':
-    check_env()
     bot = Bot()
